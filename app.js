e614c3785ff9bfed2b9c2df85fc8eac552e4350c
jeremyleitmeyer <jeremyleitmeyer@Jeremys-MacBook-Air.local>
2018-02-21 11:20:15 -0500

    added some EJS and example

--- app.js
+++ app.js
@@ -1,16 +1,21 @@
 const express = require('express');
 const app = express();
-const ejs = require('ejs');
 const expressLayouts = require('express-ejs-layouts');
 const passport = require('passport');
 const request = require('request');
 const bodyparser = require('body-parser');
 
+app.set('view engine', 'ejs')
+
 app.use(express.static('public'))
 
-app.get('/', function (req, res, next) {
+app.get('/', function (req, res) {
   // set frontend variables in here for EJS -Jeremy
-  res.sendFile(__dirname + '/index.ejs')
+  var example = 'This is cool right?'
+  res.render('pages/index', {
+    example: example
+  })
+
 })
 
 const PORT = process.env.PORT || 3000;
